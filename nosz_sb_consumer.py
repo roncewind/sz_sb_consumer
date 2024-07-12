c21fb24924e620d890d91afd65e4fb4a06e12e92
roncewind <dad@lynntribe.net>
2024-07-12 10:20:19 -0700

    updated nosz consumer

--- nosz_sb_consumer.py
+++ nosz_sb_consumer.py
@@ -145,165 +145,165 @@ try:
     with ServiceBusClient.from_connection_string(
         conn_str=connection_str
     ) as servicebus_client:
-        with servicebus_client.get_queue_receiver(
+        receiver = servicebus_client.get_queue_receiver(
             queue_name=queue_name, prefetch_count=prefetch, auto_lock_renewer=renewer
-        ) as receiver:
+        )
+        messages = 0
+        duration = 0
+        with concurrent.futures.ThreadPoolExecutor(max_workers) as executor:
 
-            messages = 0
-            duration = 0
-            with concurrent.futures.ThreadPoolExecutor(max_workers) as executor:
+            print(f"Threads: {executor._max_workers}")
+            print(f"Prefetch: {prefetch}")
+            futures = {}
+            try:
+                while True:
 
-                print(f"Threads: {executor._max_workers}")
-                print(f"Prefetch: {prefetch}")
-                futures = {}
-                try:
-                    while True:
+                    nowTime = time.time()
+                    if futures:
+                        done, _ = concurrent.futures.wait(
+                            futures,
+                            timeout=10,
+                            return_when=concurrent.futures.FIRST_COMPLETED,
+                        )
 
-                        nowTime = time.time()
-                        if futures:
-                            done, _ = concurrent.futures.wait(
-                                futures,
-                                timeout=10,
-                                return_when=concurrent.futures.FIRST_COMPLETED,
-                            )
+                        delete_batch = []
+                        delete_cnt = 0
 
-                            delete_batch = []
-                            delete_cnt = 0
-
-                            for fut in done:
-                                msg = futures.pop(fut)
-                                try:
-                                    result = fut.result()
-                                    # if result:
-                                    # print(result)  # we would handle pushing to withinfo queues here BUT that is likely a second future task/executor
-                                except (
-                                    G2RetryTimeoutExceeded,
-                                    G2BadInputException,
-                                ) as err:
-                                    print(err)
-                                    record = orjson.loads(str(msg[TUPLE_MSG]).strip())
-                                    print(
-                                        f'Sending to deadletter: {record["DATA_SOURCE"]} : {record["RECORD_ID"]}'
-                                    )
-                                    receiver.dead_letter_message(msg[TUPLE_MSG])
-                                delete_batch.append(msg[TUPLE_MSG])
-                                delete_cnt += 1
-                                if delete_cnt == 10:  # max for delete batch
-                                    # FIXME: how to delete batch in azure?
-                                    for msg in delete_batch:
-                                        # print(f"Deleting {msg}")
-                                        receiver.complete_message(msg)
-                                    delete_batch = []
-                                    delete_cnt = 0
-
-                                messages += 1
-
-                                if messages % INTERVAL == 0:  # display rate stats
-                                    diff = nowTime - prevTime
-                                    speed = -1
-                                    if diff > 0.0:
-                                        speed = int(INTERVAL / diff)
-                                    print(
-                                        f"Processed {messages} adds, {speed} records per second"
-                                    )
-                                    prevTime = nowTime
-
-                            if delete_batch:
+                        for fut in done:
+                            msg = futures.pop(fut)
+                            try:
+                                result = fut.result()
+                                # if result:
+                                # print(result)  # we would handle pushing to withinfo queues here BUT that is likely a second future task/executor
+                            except (
+                                G2RetryTimeoutExceeded,
+                                G2BadInputException,
+                            ) as err:
+                                print(err)
+                                record = orjson.loads(str(msg[TUPLE_MSG]).strip())
+                                print(
+                                    f'Sending to deadletter: {record["DATA_SOURCE"]} : {record["RECORD_ID"]}'
+                                )
+                                receiver.dead_letter_message(msg[TUPLE_MSG])
+                            delete_batch.append(msg[TUPLE_MSG])
+                            delete_cnt += 1
+                            if delete_cnt == 10:  # max for delete batch
                                 # FIXME: how to delete batch in azure?
                                 for msg in delete_batch:
                                     # print(f"Deleting {msg}")
                                     receiver.complete_message(msg)
+                                delete_batch = []
+                                delete_cnt = 0
 
-                            if nowTime > logCheckTime + (
-                                LONG_RECORD / 2
-                            ):  # log long running records
-                                logCheckTime = nowTime
+                            messages += 1
 
-                                response = bytearray()
-                                g2.stats(response)
-                                print(f"\n{response.decode()}\n")
+                            if messages % INTERVAL == 0:  # display rate stats
+                                diff = nowTime - prevTime
+                                speed = -1
+                                if diff > 0.0:
+                                    speed = int(INTERVAL / diff)
+                                print(
+                                    f"Processed {messages} adds, {speed} records per second"
+                                )
+                                prevTime = nowTime
 
-                                numStuck = 0
-                                numRejected = 0
-                                for fut, msg in futures.items():
-                                    if not fut.done():
-                                        duration = nowTime - msg[TUPLE_STARTTIME]
-                                        if duration > LONG_RECORD * (
-                                            msg[TUPLE_EXTENDED] + 1
-                                        ):
-                                            numStuck += 1
-                                            record = orjson.loads(
-                                                str(msg[TUPLE_MSG]).strip()
-                                            )
-                                            times_extended = msg[TUPLE_EXTENDED] + 1
-                                            # push out the visibility another 2 LONG_RECORD times intervals
-                                            new_time = (
-                                                times_extended + 2
-                                            ) * LONG_RECORD
-                                            receiver.renew_message_lock(msg[TUPLE_MSG])
-                                            futures[fut] = (
-                                                msg[TUPLE_MSG],
-                                                msg[TUPLE_STARTTIME],
-                                                times_extended,
-                                            )
-                                            print(
-                                                f'Extended visibility ({duration / 60:.1f} min, extended {times_extended} times): {record["DATA_SOURCE"]} : {record["RECORD_ID"]}'
-                                            )
-                                    if numStuck >= executor._max_workers:
+                        if delete_batch:
+                            # FIXME: how to delete batch in azure?
+                            for msg in delete_batch:
+                                # print(f"Deleting {msg}")
+                                receiver.complete_message(msg)
+
+                        if nowTime > logCheckTime + (
+                            LONG_RECORD / 2
+                        ):  # log long running records
+                            logCheckTime = nowTime
+
+                            response = bytearray()
+                            g2.stats(response)
+                            print(f"\n{response.decode()}\n")
+
+                            numStuck = 0
+                            numRejected = 0
+                            for fut, msg in futures.items():
+                                if not fut.done():
+                                    duration = nowTime - msg[TUPLE_STARTTIME]
+                                    if duration > LONG_RECORD * (
+                                        msg[TUPLE_EXTENDED] + 1
+                                    ):
+                                        numStuck += 1
+                                        record = orjson.loads(
+                                            str(msg[TUPLE_MSG]).strip()
+                                        )
+                                        times_extended = msg[TUPLE_EXTENDED] + 1
+                                        # push out the visibility another 2 LONG_RECORD times intervals
+                                        new_time = (times_extended + 2) * LONG_RECORD
+                                        receiver.renew_message_lock(msg[TUPLE_MSG])
+                                        futures[fut] = (
+                                            msg[TUPLE_MSG],
+                                            msg[TUPLE_STARTTIME],
+                                            times_extended,
+                                        )
                                         print(
-                                            f"All {executor._max_workers} threads are stuck on long running records"
+                                            f'Extended visibility ({duration / 60:.1f} min, extended {times_extended} times): {record["DATA_SOURCE"]} : {record["RECORD_ID"]}'
                                         )
-                        if len(futures) >= executor._max_workers + prefetch:
-                            time.sleep(1)
-                            continue
+                                if numStuck >= executor._max_workers:
+                                    print(
+                                        f"All {executor._max_workers} threads are stuck on long running records"
+                                    )
+                    if len(futures) >= executor._max_workers + prefetch:
+                        time.sleep(1)
+                        continue
 
-                        while len(futures) < executor._max_workers + prefetch:
-                            try:
-                                max_msgs = min(
-                                    10, executor._max_workers + prefetch - len(futures)
-                                )
-                                print(f"Receiving {max_msgs} messages")
-                                response = receiver.receive_messages(
-                                    max_message_count=max_msgs, max_wait_time=5
-                                )
-                                print(response)
-                                if not response:
-                                    if len(futures) == 0:
-                                        time.sleep(0.1)
-                                    break
-                                for this_msg in response:
-                                    # renewer.register(
-                                    #     receiver,
-                                    #     this_msg,
-                                    #     max_lock_renewal_duration=3600,
-                                    # )
-                                    futures[
-                                        executor.submit(
-                                            process_msg, g2, this_msg, args.info
-                                        )
-                                    ] = (this_msg, time.time(), 0)
-                            except Exception as err:
-                                print(f"{type(err).__name__}: {err}", file=sys.stderr)
-                                raise
-
-                        print(f"Processed total of {messages} adds")
-
-                except Exception as err:
-                    print(
-                        f"{type(err).__name__}: Shutting down due to error: {err}",
-                        file=sys.stderr,
-                    )
-                    traceback.print_exc()
-                    nowTime = time.time()
-                    for fut, msg in futures.items():
-                        if not fut.done():
-                            duration = nowTime - msg[TUPLE_STARTTIME]
-                            record = orjson.loads(str(msg[TUPLE_MSG]).strip())
-                            print(
-                                f'Still processing ({duration / 60:.1f} min: {record["DATA_SOURCE"]} : {record["RECORD_ID"]}'
+                    while len(futures) < executor._max_workers + prefetch:
+                        try:
+                            max_msgs = executor._max_workers + prefetch - len(futures)
+                            print(f"Receiving {max_msgs} messages")
+                            response = receiver.receive_messages(
+                                max_message_count=max_msgs, max_wait_time=5
                             )
-                    executor.shutdown()
-                    exit(-1)
+                            # print(response)
+                            if not response:
+                                if len(futures) == 0:
+                                    receiver.close()
+                                    receiver = servicebus_client.get_queue_receiver(
+                                        queue_name=queue_name,
+                                        prefetch_count=prefetch,
+                                        auto_lock_renewer=renewer,
+                                    )
+                                break
+                            for this_msg in response:
+                                # renewer.register(
+                                #     receiver,
+                                #     this_msg,
+                                #     max_lock_renewal_duration=3600,
+                                # )
+                                futures[
+                                    executor.submit(
+                                        process_msg, g2, this_msg, args.info
+                                    )
+                                ] = (this_msg, time.time(), 0)
+                        except Exception as err:
+                            print(f"{type(err).__name__}: {err}", file=sys.stderr)
+                            raise
+
+                    print(f"Processed total of {messages} adds")
+
+            except Exception as err:
+                print(
+                    f"{type(err).__name__}: Shutting down due to error: {err}",
+                    file=sys.stderr,
+                )
+                traceback.print_exc()
+                nowTime = time.time()
+                for fut, msg in futures.items():
+                    if not fut.done():
+                        duration = nowTime - msg[TUPLE_STARTTIME]
+                        record = orjson.loads(str(msg[TUPLE_MSG]).strip())
+                        print(
+                            f'Still processing ({duration / 60:.1f} min: {record["DATA_SOURCE"]} : {record["RECORD_ID"]}'
+                        )
+                executor.shutdown()
+                exit(-1)
 
     renewer.close()
 
